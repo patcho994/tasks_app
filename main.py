ae01e9f3977a5c6f153cdefb5f7bf775bb5a7033
patcho994 <patryk.chociszewski@gmail.com>
2023-05-13 09:37:34 +0200

    feat(getall): dodano nowy endpoint do pobierania zadan

--- main.py
+++ main.py
@@ -16,5 +16,13 @@ def add_task():
     return jsonify({"message": "zadanie dodane pomyslnie"}), 201
 
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    cursor = db.cursor()
+    query = "SELECT * FROM tasks"
+    cursor.execute(query)
+    tasks = cursor.fetchall()
+    return jsonify(tasks), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
